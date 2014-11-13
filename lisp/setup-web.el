644ded9a2eb77bcd639ab292ee02ecc167443e4a
Mike Yumatov <mike@yumatov.org>
2014-11-13 20:30:47 +0300

    Enable Django engine for *.html

--- lisp/setup-web.el
+++ lisp/setup-web.el
@@ -2,6 +2,9 @@
 
 (add-to-list 'auto-mode-alist '("\\.html?\\'" . web-mode))
 
+(setq web-mode-engines-alist
+      '(("django" . "\\.html?\\'")))
+
 ;; Make web-mode play nice with spartparens
 (setq web-mode-enable-auto-pairing nil)
 
