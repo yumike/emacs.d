ec899aef47d28aac84cda8de5a32d4d33c9dd5aa
Mike Yumatov <mike@yumatov.org>
2015-01-20 22:57:25 +0300

    Fix Haskell support

--- lisp/setup-haskell.el
+++ lisp/setup-haskell.el
@@ -1,4 +1,4 @@
-(require 'haskell-mode)
+(require 'haskell)
 (require 'ghc)
 (require 'company)
 (require 'company-ghc)
@@ -6,9 +6,15 @@
 (autoload 'ghc-init "ghc" nil t)
 (autoload 'ghc-debug "ghc" nil t)
 (add-hook 'haskell-mode-hook (lambda ()
+                               (interactive-haskell-mode)
                                (turn-on-haskell-indentation)
                                (ghc-init)))
 
 (add-to-list 'company-backends 'company-ghc)
+
+(setq haskell-process-type 'cabal-repl)
+
+(define-key interactive-haskell-mode-map (kbd "C-c C-c") nil)
+(define-key interactive-haskell-mode-map (kbd "C-c C-t") nil)
 
 (provide 'setup-haskell)
