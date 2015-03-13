e259d9a92a5e4bc6dc30f1b5092c156ca244295d
Mike Yumatov <mike@yumatov.org>
2015-03-13 22:27:45 +0300

    Highlight FIXME/NOTE/etc words in Python comments

--- lisp/setup-python.el
+++ lisp/setup-python.el
@@ -4,6 +4,11 @@
 (pyenv-mode)
 (add-hook 'python-mode-hook 'anaconda-mode)
 (add-hook 'python-mode-hook 'eldoc-mode)
+(add-hook 'python-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("\\<\\(FIXME\\|TODO\\|BUG\\|NOTE\\):" 1 font-lock-warning-face t)))))
 
 (add-to-list 'company-backend 'company-anaconda)
 
